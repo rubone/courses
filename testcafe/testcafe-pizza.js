1f89289bfc99cd82daa6564c45d2fa030307d13c
rubone <rubone02@gmail.com>
2022-12-27 11:56:25 -0600

    :white_check_mark: Add test pizza website testcafe

---
+++ testcafe/testcafe-pizza.js
@@ -0,0 +1,30 @@
+fixture `Pizza rubone`
+    .page `https://testcafe-demo-page.glitch.me/`;
+
+test('Submit a form', async t => {
+    await t
+        // automatically dismiss dialog boxes
+        .setNativeDialogHandler(() => true)
+
+        // drag the pizza size slider
+        .drag('.noUi-handle', 100, 0)
+
+        // select the toppings
+        .click('.next-step')
+        .click('label[for="pepperoni"]')
+        .click('#step2 .next-step')
+
+        // fill the address form
+        //.click('.confirm-address') Doesn't work
+        .typeText('#address', 'My address')
+        .typeText('#phone-input', '+1-541-754-3001')
+        .click('#step3 .next-step')
+
+        // zoom into the iframe map
+        .switchToIframe('.restaurant-location iframe')
+        .click('button[title="Zoom in"]')
+
+        // submit the order
+        .switchToMainWindow()
+        .click('.complete-order');
+});
